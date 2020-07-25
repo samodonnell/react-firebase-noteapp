4de815ad5f8e338439fb912f593ed37392b75fcd
Sam O'Donnell <samuel.odonnell1@gmail.com>
2020-07-25 11:49:56 +1000

    Implemented basic functionality, add or remove notes, connected to firebase

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header className='header'>
+      <h3 className='title'>Note Taker</h3>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyARULYLFl94OutNba3Yde2XgfwddBoi16w',
+  authDomain: 'react-notes-app-c072e.firebaseapp.com',
+  databaseURL: 'https://react-notes-app-c072e.firebaseio.com',
+  projectId: 'react-notes-app-c072e',
+  storageBucket: 'react-notes-app-c072e.appspot.com',
+  messagingSenderId: '321553553030',
+  appId: '1:321553553030:web:4a17e49a451e58eb0a6307',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
